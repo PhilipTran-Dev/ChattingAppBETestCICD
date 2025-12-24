cb291e0669cd216fe9f274a2a24eb5dcee2837c1
Phuc <Phuc@example.com>
2025-12-24 21:51:15 +0700

    seven time

--- src/main/java/com/ChattingApp/ChattingAppBackend/Controller/HealthyController.java
+++ src/main/java/com/ChattingApp/ChattingAppBackend/Controller/HealthyController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthyController {
     @GetMapping("/health")
     public String health() {
-        return "OK I'm done cicd github actions with ec2 done";
+        return "Ok i need learn cicd s3 FE reactjs";
     }
 }
